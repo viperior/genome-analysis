86df6a8de6348b14134f7b37868658be5d4168d0
viperior <viperior@gmail.com>
2019-11-27 03:50:28 +0000

    Refactor data extraction logic into more modular style; add the creation of a sample output file of truncated length.

--- extract.py
+++ extract.py
@@ -1,14 +1,25 @@
-input_file_path = './input/ManuSporny-genome.txt'
-output_file_path = './output/genome.txt'
-
-with open(input_file_path, 'r') as input_file:
-    i = 0
-    open(output_file_path, 'w').close()
+def copy_file_data(source_file_path, target_file_path, start_line_number=0, end_line_number=False):
+    erase_file_contents(target_file_path)
     
-    for line in input_file:
-        i += 1
+    with open(source_file_path, 'r') as source_file:
+        i = 0
         
-        if(i <= 100):
-            print(line.strip())
-            with open(output_file_path, 'a') as output_file:
-                output_file.write(line)
+        for line in source_file:
+            if(i > start_line_number and ((end_line_number == False) or i <= end_line_number)):
+                with open(target_file_path, 'a') as target_file:
+                    target_file.write(line)
+            i += 1
+
+def erase_file_contents(file_path):
+    open(file_path, 'w').close()
+
+def extract_data():
+    input_file_path = './input/ManuSporny-genome.txt'
+    input_file_header_line_count = 14
+    input_file_data_start_line_number = input_file_header_line_count + 1
+    output_sample_data_file_path = './output/genome-sample.txt'
+    output_data_file_path = './output/genome.txt'
+    copy_file_data(input_file_path, output_sample_data_file_path, start_line_number=input_file_data_start_line_number, end_line_number=300)
+    copy_file_data(input_file_path, output_data_file_path, start_line_number=input_file_data_start_line_number, end_line_number=3000)
+
+extract_data()
